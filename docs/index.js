b55eb9b13b5a7e82f48600a1e5d6444b55ad59b5
Danny Schumacher <daniel.r.schumacher@uscis.dhs.gov>
2018-11-28 13:51:32 -0500

    wip

--- docs/index.js
+++ docs/index.js
@@ -1,28 +1,29 @@
 (($) => {
-
     $(document).ready(() => {
         $('select').select2({tags: true, createTag: (params) => { return undefined }})
         
-        $.each(items, (idx, item) => {
-            $("#item").append($(`<option value="${item.c}">${item.l}</option>`))
-        })
-        $.each(qualities, (idx, quality) => {
-            $("#quality").append($(`<option value="${quality.v}">${quality.l}</option>`))
-        })
-        $.each(sockets, (idx, socket) => {
-            $("#socket").append($(`<option value="${socket.v}">${socket.l}</option>`))
+        //this is lazy and not how things should be done
+        $('select').each((idx, select) => {
+            $.each(window[select.id], (idx, item) => {
+                $(`#${select.id}`).append($(`<option value="${item.v}">${item.l}</option>`))
+            })
         })
     })
-
     $(document).on('change', 'select', () => {
         var code = " "
-        $('#item, #quality').each((idx, select) => {
+
+        //again... lazy
+        $('select').each((idx, select) => {
             var val = $(select).val()
-            if(val && val.length) { code = code + " " + $(select).val().join(" ") }
-        })
-        $('#socket').each((idx, select) => {
-            var val = $(select).val()
-            if(val && val.length) { code = code + " (" + val.join(" OR ") + ")" }
+            var or = $(select).hasClass('or')
+            if(val && val.length) { 
+                if(or) {
+                    code = val.length > 1 ? `${code} (${val.join(" OR ")})` : `${code} ${val}`
+                } else {
+                    code = val.length > 1 ? `${code} (${val.join(" AND ")})` : `${code} ${val}`
+                }
+            }
+            if(val && val.length) {  }
         })
         $("#output").text(`ItemDisplay[${code.trim()}]: %NAME%`)
     })
